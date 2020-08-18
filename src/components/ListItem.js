bdda377acae7e4113e6328069e3caf5a2013e6f6
manuelsanchez2 <manusansan22@gmail.com>
2020-08-18 15:34:40 +0200

    Improve layout of the list items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./ListItem.css";
 
 function ListItem({ children }) {
-  return <a href="#hola">{children}</a>;
+  return (
+    <a className="listItem" href="#hola">
+      {children}
+    </a>
+  );
 }
 
 export default ListItem;
