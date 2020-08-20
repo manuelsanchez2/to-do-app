53d0846c3eacd05a78c3d86740114c959727b363
manuelsanchez2 <manusansan22@gmail.com>
2020-08-20 14:07:13 +0200

    Adapt ListItem to JSS

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,28 @@
 import React from "react";
-import "./ListItem.css";
+import styled from "@emotion/styled";
+
+const ListContainer = styled.a`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  padding: 15px 0;
+  text-decoration: none;
+  border-bottom: 1px solid black;
+
+  *:first-child {
+    margin: 0 10px;
+  }
+  *:last-child {
+    margin: 0 15px;
+  }
+  h3 {
+    margin: 0;
+  }
+`;
 
 function ListItem({ children }) {
-  return (
-    <a className="listItem" href="#hola">
-      {children}
-    </a>
-  );
+  return <ListContainer href="#hola">{children}</ListContainer>;
 }
 
 export default ListItem;
