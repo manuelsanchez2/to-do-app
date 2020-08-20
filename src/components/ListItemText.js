48da27099f45974e5e20e8f15d98df576fb789d9
manuelsanchez2 <manusansan22@gmail.com>
2020-08-20 13:54:38 +0200

    Add font Nunito and improve LitItemText

--- src/components/ListItemText.js
+++ src/components/ListItemText.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./ListItemText.css";
+import styled from "@emotion/styled";
+
+const Title = styled.h3`
+  color: var(--highlight-Color);
+  font-weight: bolder;
+`;
+
+const SubTitle = styled.span`
+  color: var(--highlight-Color);
+  font-weight: bolder;
+  font-style: italic;
+`;
+
+const SubSubTitle = styled.span`
+  display: block;
+`;
 
 function ListItemText({ title, author, date }) {
   return (
     <div>
-      <h3>{title}</h3>
-      <span>{author}</span>
-      <span className="listItemText__date">{date}</span>
+      <Title>{title}</Title>
+      <SubTitle>{author}</SubTitle>
+      <SubSubTitle>{date}</SubSubTitle>
     </div>
   );
 }
